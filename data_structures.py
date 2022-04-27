2330d8d70d9985a543d67ad82e6fb29d389949a0
Akinyeye Samuel <akinsam23@gmail.com>
2022-04-28 00:53:06 +0100

    Update data_structures.py
    Added another quiz

--- data_structures.py
+++ data_structures.py
@@ -121,6 +121,20 @@ print(boy_names[2013].keys())
 for year in boy_names:
     # Safely print the year and the third ranked name or 'Unknown'
     print(year, boy_names[year].get(3, 'Unkonw'))
+ 
+#ex3.5
+# Assign the names_2011 dictionary as the value to the 2011 key of boy_names
+boy_names[2011] = names_2011
+
+# Update the 2012 key in the boy_names dictionary
+boy_names[2012].update([(1, 'Casey'), (2, 'Aiden')])
+
+# Loop over the years in the boy_names dictionary 
+for year in boy_names:
+    # Sort the data for each year by descending rank and get the lowest one
+    lowest_ranked = sorted(boy_names[year], reverse=True)[0]
+    # Safely print the year and the least popular name or 'Not Available'
+    print(year, boy_names[year].get(lowest_ranked, 'Not Available'))
 
 #ex4
 # Remove 2011 and store it: female_names_2011
